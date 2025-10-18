7e384595dccd19c6f9638eb0cb7dbe2377151fff
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-10-19 00:25:59 +0800

    draft the main task to achieve

--- MyAsyncAwait/Program.cs
+++ MyAsyncAwait/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using MyAsyncAwait;
+
 Console.WriteLine("Hello, World!");
+
+return;
+
+MyTask PaoQuan(CancellationToken token)
+{
+    return new MyTask();
+}
+
+MyTask<string> ReadFile(string filePath)
+{
+    return new MyTask<string>();
+}
+
+IEnumerable<MyTask> MainTaskImpl(MyWritableTask resultTask)
+{
+    Console.WriteLine("MyWritableTask test process");
+
+    using var paoQuanTokenSource = new CancellationTokenSource();
+    var paoQuanTask = PaoQuan(paoQuanTokenSource.Token);
+
+    Console.WriteLine("Input a filename, otherwise I will continue Pao Quan.");
+    var filePath = Console.ReadLine() ?? "";
+    var readFileTask = ReadFile(filePath);
+    yield return readFileTask;
+    var fileContent = readFileTask.Result;
+    Console.WriteLine("I see the file has:\n" + fileContent);
+
+    paoQuanTokenSource.Cancel();
+    yield return paoQuanTask;
+    paoQuanTask.EnsureComplete();
+
+    resultTask.SetResult();
+}
