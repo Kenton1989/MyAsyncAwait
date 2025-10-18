f647a5e44a185800c657a5dd66d01cd8cc83606a
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-10-18 22:58:21 +0800

    create task classes

--- MyAsyncAwait/MyTask.cs
+++ MyAsyncAwait/MyTask.cs
@@ -1,6 +1,29 @@
 ﻿namespace MyAsyncAwait;
+
 public class MyTask
 {
     public bool IsCompleted { get; protected set; }
+}
+
+public class MyTask<TResult>: MyTask
+{
+    public TResult? Result { get; protected set; }
+}
+
+public class MyWritableTask : MyTask
+{
+    public void SetResult()
+    {
+        IsCompleted = true;
+    }
+}
+
+public class MyWritableTask<TResult> : MyTask<TResult>
+{
+    public void SetResult(TResult result)
+    {
+        IsCompleted = true;
+        Result  = result;
+    }
 }
 
--- MyAsyncAwaitTest/TaskTest.cs
+++ MyAsyncAwaitTest/TaskTest.cs
@@ -16,4 +16,38 @@ public class TaskTest
         var task = new MyTask();
         task.IsCompleted.Should().BeFalse();
     }
+
+    [Test]
+    public void CompletTaskTest()
+    {
+        var writableTask = new MyWritableTask();
+        MyTask task = writableTask;
+
+        task.IsCompleted.Should().BeFalse();
+
+        writableTask.SetResult();
+
+        task.IsCompleted.Should().BeTrue();
+    }
+
+    [Test]
+    public void IncompletTaskWithResultTest()
+    {
+        var task = new MyTask<int>();
+
+        task.IsCompleted.Should().BeFalse();
+        task.Result.Should().Be(0);
+    }
+
+    [Test]
+    public void CompletTaskWithResultTest()
+    {
+        var writableTask = new MyWritableTask<int>();
+        MyTask<int> task = writableTask;
+
+        writableTask.SetResult(1);
+
+        task.IsCompleted.Should().BeTrue();
+        task.Result.Should().Be(1);
+    }
 }
