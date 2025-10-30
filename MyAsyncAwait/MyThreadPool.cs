b6c5006144e98f2d96284793aad22b61b3f7775f
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-10-31 00:03:11 +0800

    draft MyThreadPool interface

---
+++ MyAsyncAwait/MyThreadPool.cs
@@ -0,0 +1,15 @@
+﻿namespace MyAsyncAwait;
+
+public class MyThreadPool
+{
+    public static MyThreadPool Instance { get; } = new MyThreadPool();
+
+    private MyThreadPool(int threadCount = 4)
+    {
+    }
+
+    public void QueueWork(Action func)
+    {
+        func();
+    }
+}
---
+++ MyAsyncAwaitTest/ThreadPoolTest.cs
@@ -0,0 +1,34 @@
+﻿using AwesomeAssertions;
+using MyAsyncAwait;
+
+namespace MyAsyncAwaitTest;
+
+public class ThreadPoolTest
+{
+    private MyThreadPool _target;
+
+    [SetUp]
+    public void Setup()
+    {
+        _target = MyThreadPool.Instance;
+    }
+
+    [Test]
+    public void RunOneTask()
+    {
+        var passSignal = new ManualResetEvent(false);
+        
+        _target.QueueWork(TestFunc);
+        
+        var pass = passSignal.WaitOne(TimeSpan.FromSeconds(1));
+        
+        pass.Should().BeTrue();
+
+        return;
+
+        void TestFunc()
+        {
+            passSignal.Set();
+        }
+    }
+}
