610d11cf17176fc887b2fd4ddf88ac5377af9234
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-11-01 01:03:49 +0800

    created my thread pool

--- MyAsyncAwait/MyThreadPool.cs
+++ MyAsyncAwait/MyThreadPool.cs
@@ -1,15 +1,85 @@
-﻿namespace MyAsyncAwait;
+﻿using System.Collections.Concurrent;
 
-public class MyThreadPool
+namespace MyAsyncAwait;
+
+public class MyThreadPool : IDisposable
 {
-    public static MyThreadPool Instance { get; } = new MyThreadPool();
+    public static MyThreadPool Instance { get; } = new();
 
-    private MyThreadPool(int threadCount = 4)
+    private readonly Thread[] _workerThreads;
+    private readonly ConcurrentQueue<Action> _workItems = new();
+    private readonly AutoResetEvent _workItemQueued = new(false);
+
+    private MyThreadPool(int threadCount = 8)
     {
+        _workerThreads = Enumerable.Range(0, threadCount)
+            .Select(CreateThread)
+            .ToArray();
+    }
+
+    private Thread CreateThread(int idx)
+    {
+        var thread = new Thread(Work)
+        {
+            IsBackground = true,
+            Name = $"{nameof(MyThreadPool)}.Worker.{idx}",
+            Priority = ThreadPriority.Normal
+        };
+
+        thread.Start();
+        return thread;
     }
 
     public void QueueWork(Action func)
     {
-        func();
+        _workItems.Enqueue(func);
+        _workItemQueued.Set();
+    }
+
+    private void Work(object? obj)
+    {
+        while (!IsDisposed)
+        {
+            try
+            {
+                if (_workItems.TryDequeue(out var workItem))
+                {
+                    workItem();
+                }
+                else
+                {
+                    _workItemQueued.WaitOne(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception: {1}", Thread.CurrentThread.Name, ex);
+            }
+        }
+    }
+
+    private int _disposedFlag;
+    private bool IsDisposed => _disposedFlag == 1;
+    private bool TrySetDisposed()
+    {
+        var oldValue = Interlocked.CompareExchange(ref _disposedFlag, 1, 0);
+        return oldValue == 0;
+    }
+
+    public void Dispose()
+    {
+        if (TrySetDisposed())
+        {
+            foreach (var workerThread in _workerThreads)
+            {
+                workerThread.Join();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+        else
+        {
+            throw new ObjectDisposedException(nameof(MyThreadPool));
+        }
     }
 }
--- MyAsyncAwaitTest/ThreadPoolTest.cs
+++ MyAsyncAwaitTest/ThreadPoolTest.cs
@@ -1,5 +1,6 @@
 ﻿using AwesomeAssertions;
 using MyAsyncAwait;
+using NUnit.Framework.Internal;
 
 namespace MyAsyncAwaitTest;
 
@@ -21,7 +22,7 @@ public class ThreadPoolTest
         _target.QueueWork(TestFunc);
         
         var pass = passSignal.WaitOne(TimeSpan.FromSeconds(1));
-        
+
         pass.Should().BeTrue();
 
         return;
@@ -29,6 +30,44 @@ public class ThreadPoolTest
         void TestFunc()
         {
             passSignal.Set();
+        }
+    }
+
+    [Test]
+    public void RunTwoTask()
+    {
+        var testDelay = TimeSpan.FromSeconds(1);
+        var testPrecision = TimeSpan.FromMilliseconds(100);
+
+        var startTime = DateTimeOffset.Now;
+
+        var signal1 = new ManualResetEvent(false);
+        _target.QueueWork(() => SleepAndSet(signal1));
+
+        var signal2 = new ManualResetEvent(false);
+        _target.QueueWork(() => SleepAndSet(signal2));
+
+        var queueTime = DateTimeOffset.Now;
+        
+        var func1Completed = signal1.WaitOne(TimeSpan.FromSeconds(3));
+        var func2Completed = signal2.WaitOne(TimeSpan.FromSeconds(3));
+
+        var completeTime = DateTimeOffset.Now;
+        
+        var queueDuration = queueTime - startTime;
+        var executeDuration = completeTime - startTime;
+        
+        func1Completed.Should().BeTrue();
+        func2Completed.Should().BeTrue();
+        queueDuration.Should().BeCloseTo(TimeSpan.Zero, testPrecision);
+        executeDuration.Should().BeCloseTo(testDelay, testPrecision);
+
+        return;
+
+        void SleepAndSet(ManualResetEvent resetEvent)
+        {
+            Thread.Sleep(testDelay);
+            resetEvent.Set();
         }
     }
 }
